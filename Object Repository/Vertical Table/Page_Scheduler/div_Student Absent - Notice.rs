e50f00c590fbf0012ab992b8a7bf5f1fa0ce88d0
eriborromeo <emsbdev@gmail.com>
2024-10-14 10:18:10 +0800

    10/14 added new test scripts

---
+++ Object Repository/Vertical Table/Page_Scheduler/div_Student Absent - Notice.rs
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Student Absent - Notice</name>
+   <tag></tag>
+   <elementGuidId>a2228844-9e98-4ce5-8993-95baad8676c0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#tr-main-4 > td.border-stone-700.border-2.px-2.bg-gray-900.text-white > div.providerName.table-cell.cursor-pointer.align-middle</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//tr[@id='tr-main-4']/td[5]/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>#tr-main-4 >> internal:text=&quot;Student Absent - Notice&quot;i</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>308e5502-dbad-4072-bef8-47ecd553e7c2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>providerName table-cell cursor-pointer align-middle</value>
+      <webElementGuid>f43c71d2-c21f-4275-9784-1f71364b6751</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Student Absent - Notice</value>
+      <webElementGuid>d88da7e6-ba44-4d2b-bcc0-ccabb57a6098</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;tr-main-4&quot;)/td[@class=&quot;border-stone-700 border-2 px-2  bg-gray-900 text-white&quot;]/div[@class=&quot;providerName table-cell cursor-pointer align-middle&quot;]</value>
+      <webElementGuid>dd4235dd-ea06-4fc6-859e-7628f137c5e7</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//tr[@id='tr-main-4']/td[5]/div</value>
+      <webElementGuid>1c39046c-3035-4e77-8678-576187a6c029</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Mon, 10/14 07:30 - 13:30'])[1]/following::div[2]</value>
+      <webElementGuid>998ec44b-d2de-4ef5-937a-8644aa7ad28e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Ledo Glade, Paloma'])[2]/following::div[2]</value>
+      <webElementGuid>8b5cc94a-1716-485c-b020-4c55903fe630</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Aliiolani Elementary'])[5]/preceding::div[1]</value>
+      <webElementGuid>4a2b17b0-7be1-4b84-971e-345a9274c256</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Mcelfresh, Helixorion'])[2]/preceding::div[1]</value>
+      <webElementGuid>0f06e528-71c0-44e4-a251-e883cbcc19b1</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//tr[6]/td[5]/div</value>
+      <webElementGuid>b1d31a19-3ef3-4d79-8b6e-d69237cb5624</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Student Absent - Notice' or . = 'Student Absent - Notice')]</value>
+      <webElementGuid>35e23a63-2f61-471b-9bf7-e6d62639422b</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
