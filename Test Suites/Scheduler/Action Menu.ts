7524a414ebe65db41cfc061fe67edb8e239ca9b8
eriborromeo <emsbdev@gmail.com>
2024-10-24 14:10:53 +0800

    erica 10/24 fixed folders

--- Test Suites/Scheduler/Action Menu.ts
+++ Test Suites/Scheduler/Action Menu.ts
@@ -15,21 +15,21 @@
       <guid>16b59c57-2283-4913-948d-991a74122269</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scheduler/Action Menu/Action Menu - Dropdown - Complete List Displayed</testCaseId>
+      <testCaseId>Test Cases/Scheduler/Vertical - Action Menu/Action Menu - Dropdown - Complete List Displayed</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>78465adc-272d-48e0-8839-1d4ba03458ee</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scheduler/Action Menu/Action Menu - Dropdown - Disabled if Master Checkbox Clear</testCaseId>
+      <testCaseId>Test Cases/Scheduler/Vertical - Action Menu/Action Menu - Dropdown - Disabled if Master Checkbox Clear</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>b04604ac-ec3d-4933-8f81-12943fbd3934</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scheduler/Action Menu/Action Menu - Dropdown - Enabled if Master Checkbox Set</testCaseId>
+      <testCaseId>Test Cases/Scheduler/Vertical - Action Menu/Action Menu - Dropdown - Enabled if Master Checkbox Set</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
--- Test Suites/Scheduler/Login.ts
+++ Test Suites/Scheduler/Login.ts
@@ -15,112 +15,112 @@
       <guid>081cbec1-3d35-4c3b-909e-c20a2295de9c</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scheduler/Login/Login - Verify Load - All Dropdown Clickable</testCaseId>
+      <testCaseId>Test Cases/Scheduler/Vertical/Login/Login - Verify Load - All Dropdown Clickable</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>4c2b3887-1e82-4328-9695-cd672e1e838e</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scheduler/Login/Login - Verify Load - Bookmark Clickable</testCaseId>
+      <testCaseId>Test Cases/Scheduler/Vertical/Login/Login - Verify Load - Bookmark Clickable</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>d38462c7-ca7e-436f-a463-d6e72526930a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scheduler/Login/Login - Verify Load - Date Dropdown Clickable</testCaseId>
+      <testCaseId>Test Cases/Scheduler/Vertical/Login/Login - Verify Load - Date Dropdown Clickable</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>7e0ea64d-8bdd-453a-bab2-4cd1737e2377</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scheduler/Login/Login - Verify Load - End Time</testCaseId>
+      <testCaseId>Test Cases/Scheduler/Vertical/Login/Login - Verify Load - End Time</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>de09e9c3-0723-486f-95ae-08be51f6295b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scheduler/Login/Login - Verify Load - List of Locations</testCaseId>
+      <testCaseId>Test Cases/Scheduler/Vertical/Login/Login - Verify Load - List of Locations</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>fd330b89-0c83-40c6-a3ed-7065c61fa45b</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scheduler/Login/Login - Verify Load - Locations Clickable</testCaseId>
+      <testCaseId>Test Cases/Scheduler/Vertical/Login/Login - Verify Load - Locations Clickable</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>74f6aeb0-7a60-4530-aa3e-acaa64df0986</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scheduler/Login/Login - Verify Load - No Missing End Time</testCaseId>
+      <testCaseId>Test Cases/Scheduler/Vertical/Login/Login - Verify Load - No Missing End Time</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>0f8619c3-ca10-45dc-8707-d38651f3e556</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scheduler/Login/Login - Verify Load - No Missing School</testCaseId>
+      <testCaseId>Test Cases/Scheduler/Vertical/Login/Login - Verify Load - No Missing School</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>8ea4665e-2c07-4bd9-8054-20f76f9479bd</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scheduler/Login/Login - Verify Load - No Missing Start Time</testCaseId>
+      <testCaseId>Test Cases/Scheduler/Vertical/Login/Login - Verify Load - No Missing Start Time</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>6f75cb19-e55b-4693-9294-e05082e0e7ed</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scheduler/Login/Login - Verify Load - No Missing Student</testCaseId>
+      <testCaseId>Test Cases/Scheduler/Vertical/Login/Login - Verify Load - No Missing Student</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>e4e3abdf-181c-4ea2-8a72-bd88ae2a38f8</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scheduler/Login/Login - Verify Load - Provider Dropdown Clickable</testCaseId>
+      <testCaseId>Test Cases/Scheduler/Vertical/Login/Login - Verify Load - Provider Dropdown Clickable</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>ee6ce183-4e77-461e-821a-a0220a423520</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scheduler/Login/Login - Verify Load - School Dropdown Clickable</testCaseId>
+      <testCaseId>Test Cases/Scheduler/Vertical/Login/Login - Verify Load - School Dropdown Clickable</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>50c1345b-54bd-4870-86b8-b73546452ce2</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scheduler/Login/Login - Verify Load - Search Clickable</testCaseId>
+      <testCaseId>Test Cases/Scheduler/Vertical/Login/Login - Verify Load - Search Clickable</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>8bf4223a-d185-42ed-88d2-69276761bd33</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scheduler/Login/Login - Verify Load - Start Time</testCaseId>
+      <testCaseId>Test Cases/Scheduler/Vertical/Login/Login - Verify Load - Start Time</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>a59ce44d-0af3-4996-b921-67addc371dde</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scheduler/Login/Login - Verify Load - Student Dropdown Clickable</testCaseId>
+      <testCaseId>Test Cases/Scheduler/Vertical/Login/Login - Verify Load - Student Dropdown Clickable</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>64c13ef0-a09a-4a1c-9156-e09b89b56ed3</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Scheduler/Login/Login - Verify Load - Title equals Scheduler</testCaseId>
+      <testCaseId>Test Cases/Scheduler/Vertical/Login/Login - Verify Load - Title equals Scheduler</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
